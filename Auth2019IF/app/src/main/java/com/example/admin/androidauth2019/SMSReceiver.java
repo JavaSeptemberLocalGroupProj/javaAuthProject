bf05416db3a3b6b6d6741b55a1d35483830a7751
Yehor <admin@MacBook-Pro-Yehor.local>
2019-01-07 23:50:08 +0200

    Catch_Message_Added

---
+++ Auth2019IF/app/src/main/java/com/example/admin/androidauth2019/SMSReceiver.java
@@ -0,0 +1,17 @@
+package com.example.admin.androidauth2019;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+
+public class SMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        System.out.println("Hello World!!!!!!");
+        SmsMessage sm = Telephony.Sms.Intents.getMessagesFromIntent(intent)[0];
+        System.out.println("Yess: " + sm.getMessageBody());
+
+    }
+}
